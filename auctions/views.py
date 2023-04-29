144a92701e7bc75e092c94610a457159a630d8fb
Federico Perez Cusse <fepc94@gmail.com>
2023-04-29 14:25:10 -0300

    Implemantation of Auction Model and Create New Listing.

--- auctions/views.py
+++ auctions/views.py
@@ -3,6 +3,8 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from .forms import NewListing
 
 from .models import User
 
@@ -61,3 +63,16 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+@login_required
+def new_listing(request):
+    if request.method == 'POST':
+        form = NewListing(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        form = NewListing()
+    
+    return render(request, 'auctions/new_listing.html', {'form' : form})
