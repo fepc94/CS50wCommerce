8a9b2d0758b551168af03c3f10d12ed9434b802b
Federico Perez Cusse <fepc94@gmail.com>
2023-07-17 16:27:45 -0300

    Final Front end improvements

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,22 +1,31 @@
-from django.forms import ModelForm
+from django import forms
 from auctions.models import AuctionListing, Bid, Comment
 
 # Create the form class for auction listing
-class NewListing(ModelForm):
+class NewListing(forms.ModelForm):
     class Meta:
         model = AuctionListing
         fields = ['title', 'description', 'start_bid', 'image_url', 'category']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'start_bid': forms.NumberInput(attrs={'class': 'form-control'}),
+            'image_url': forms.URLInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+        }
 
-class PlaceBid(ModelForm):
+class PlaceBid(forms.ModelForm):
     class Meta:
         model = Bid
         fields = ['place_bid']
+        widgets = {
+            'place_bid': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
 
-class AddComment(ModelForm):
+class AddComment(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['comment']
-
-'''
-TODO: https://www.youtube.com/watch?v=6-XXvUENY_8 Para hacer el FRONTEND
-'''
+        widgets = {
+            'comment': forms.Textarea(attrs={'class': 'form-control'}),
+        }
